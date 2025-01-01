2387ac845756943de90dedc66136edf1801691f6
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-01 14:32:19 +0600

    Create Basic(Input, output, Variable, Datatype, Type Casting).cs

---
+++ C#/Basic(Input, output, Variable, Datatype, Type Casting).cs
@@ -0,0 +1,53 @@
+// First Program run using C#
+// using System;
+
+// class Test {
+//     public static void Main(string [] args) {
+//         string s; 
+//         s = "Hafiz Ahmed";
+//         Console.WriteLine(s);
+//     }
+// }
+
+using System;
+class Test
+{
+    public static void Main(string[] args)
+    {
+        // type casting 
+        // int salary = 25437;
+        // Console.WriteLine(salary);
+        // Console.WriteLine(Convert.ToString(salary));
+
+        // parse and try Parse
+        // string s;
+        // s = "2342";
+        // int num = int.Parse(s);
+        // Console.WriteLine($"The Number is: : {num}");
+
+        // string s = "10";
+        // int res;
+        // bool ok = int.TryParse(s, out res);
+        // Console.WriteLine($"The program success!!");
+        // Console.WriteLine($"The res is: {res}");
+
+
+        // take input and output
+        // string? name = Console.ReadLine();
+        // Console.WriteLine($"My name is: " + name);
+        // int age;
+        // age = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("My age is: " + age);
+
+        // Area of triange
+        // Console.WriteLine("Calculation Area of Circle");
+
+        // Console.WriteLine("Enter radius: ");
+        // double radius = Convert.ToDouble(Console.ReadLine());
+
+        // double ans = 3.1416 * radius * radius;
+
+        // Console.WriteLine($"Circle are is:{ans.ToString("F2")}");
+
+    }
+}
