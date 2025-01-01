1f4eba058bcccf152f3f512a69b4aa80decb1125
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-01 17:47:09 +0600

    Create IF - Else & Switch.cs

---
+++ C#/IF - Else & Switch.cs
@@ -0,0 +1,46 @@
+// Basics of IF- Else 
+// using System;
+
+// class Test {
+//     public static void Main(string [] args) {
+//         int age = 23; 
+//         if(age >= 18) {
+//             Console.WriteLine("You can vote!!");
+//         }
+//         else {
+//             Console.WriteLine("You cannot vote");
+//         }
+//     }
+// }
+
+// switch case
+
+
+using System;
+using System.Diagnostics.Metrics;
+class Test {
+    public static void Main(string [] args) {
+        // int digit = 12;
+        // string message = digit switch 
+        // {
+        //     1 => "One",
+        //     2 => "two",
+        //     3 => "three",
+        //     4 => "Four",
+        //     _ => "This is invalid"
+        // };
+
+        // Console.WriteLine(message);
+
+        // object name = "Hafiz";
+        // string message = name switch {
+        //     int => "Integer",
+        //     double => "Double",
+        //     string => "String",
+        //     float => "Float",
+        //     _ => "Invalid Data type"
+        // };
+
+        // Console.WriteLine(message);
+    }
+}
