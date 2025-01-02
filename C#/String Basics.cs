dfdf4b9041361feb8f06686c666f4ba415ed5713
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-02 17:53:06 +0600

    Update String Basics.cs

--- C#/String Basics.cs
+++ C#/String Basics.cs
@@ -4,33 +4,45 @@ using System;
 
 class Test {
     public static void Main(string[] args) {
-        string? s = Console.ReadLine();
+        // string? s = Console.ReadLine();
 
-        //Console.WriteLine($"Length of the string is: {s.Length}");
+        // //Console.WriteLine($"Length of the string is: {s.Length}");
 
-        bool ok = string.IsNullOrEmpty(s);
-        Console.WriteLine($"Is the string is Null or Empty: {ok}");
+        // bool ok = string.IsNullOrEmpty(s);
+        // Console.WriteLine($"Is the string is Null or Empty: {ok}");
 
-        string upper = s.ToUpper();
-        Console.WriteLine($"The string is: {upper}");
-        string lower = s.ToLower();
-        Console.WriteLine($"The string is: {lower}");
-
-
-        string TrimString = s.Trim();
-        Console.WriteLine($"The string is: {TrimString}");
-
-        string SubStr = s.Substring(3);
-        Console.WriteLine($"The string is: {SubStr}");
+        // string upper = s.ToUpper();
+        // Console.WriteLine($"The string is: {upper}");
+        // string lower = s.ToLower();
+        // Console.WriteLine($"The string is: {lower}");
 
 
-        string revstr = new string(s.Reverse().ToArray());
-        Console.WriteLine($"The string is: {revstr}");
+        // string TrimString = s.Trim();
+        // Console.WriteLine($"The string is: {TrimString}");
 
-        var word = s.Split(" ");
-        foreach(var x in word) {
-            Console.Write(x + " ");
-        }
+        // string SubStr = s.Substring(3);
+        // Console.WriteLine($"The string is: {SubStr}");
+
+
+        // string revstr = new string(s.Reverse().ToArray());
+        // Console.WriteLine($"The string is: {revstr}");
+
+        // var word = s.Split(" ");
+        // foreach(var x in word) {
+        //     Console.Write(x + " ");
+        // }
+
+        string s = "My name is Hafiz";
+        int cnt_vowel = s.Count(c => "aeiouAEIOU".Contains(c));
+        Console.WriteLine("Number of Vowels: " + cnt_vowel);
+
+        int cnt_consonant = s.Count(c => char.IsLetter(c) && !"aeiouAEIOU".Contains(c));
+        Console.WriteLine("Number of Consonant: " + cnt_consonant);
+
+        int digit = s.Count(char.IsDigit);
+        Console.WriteLine("Number of Digits: " + digit);
+        int special_char = s.Count(c => !char.IsLetterOrDigit(c) && !char.IsWhiteSpace(c));
+        Console.WriteLine("Number of Special Character: " + cnt_consonant);  
 
     }
 }
