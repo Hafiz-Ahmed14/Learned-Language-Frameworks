1476567ed6a880f252d30b6d9b5dd5c2fe020bd7
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-06 14:04:33 +0600

    Create Basic Structure of Web Api.cs

---
+++ ASP.NET/Basic Structure of Web Api.cs
@@ -0,0 +1,17 @@
+// Basic of API Create
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+app.MapGet("/hello", () =>
+{
+    return "Hello";
+});
+
+app.MapGet("/hafiz", () =>
+{
+    return "HI am Hafiz";
+});
+app.Run();
