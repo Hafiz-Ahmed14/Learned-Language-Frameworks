627d2552da4722c18dd650b1d2c688b20f0ec084
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-11 23:52:30 +0600

    Add Dependency and Injection file

---
+++ ASP.NET/Master Services & Dependency Injection/Ecomerce/Program.cs
@@ -0,0 +1,63 @@
+//Program.cs
+using Ecomerce.Controllers;
+using Ecomerce.Services;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<CategoryService>();
+
+// Add Services to the Controller
+
+builder.Services.AddControllers();
+
+builder.Services.Configure<ApiBehaviorOptions>(options => {
+    options.InvalidModelStateResponseFactory = context => {
+
+        var errors = context.ModelState.Where(e => e.Value != null && e.Value.Errors.Count > 0)
+                .SelectMany(e => e.Value?.Errors != null ? e.Value.Errors.Select(x => x.ErrorMessage): new List<string>()).ToList();
+
+        return new BadRequestObjectResult(ApiResponse<object>.ErrorResponse(errors, 400, "Validation Failed"));
+    };
+});
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+// Check the apps working or not
+app.MapGet("/", () =>
+{
+    return "App is working Well!!";
+});
+
+
+// Create a list
+
+// See the initialize situation of list
+
+
+// Show now the list output by search wise
+
+
+// Created a Manual array
+
+
+// create dinamically
+
+
+
+// Delete 
+
+// Update
+
+// Mapping
+app.MapControllers();
+
+app.Run();
